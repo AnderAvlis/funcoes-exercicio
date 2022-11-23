4cf7502f89db548bdcfd65060484a66c5c8f3cf8
anderavlis <andersonyarro0@gmail.com>
2022-11-23 14:19:30 -0300

    criado 4 funcoes, adicionado funçao para questionar ao usuario o valor que sera aplicado as funcoes

---
+++ exercicio3/script.js
@@ -0,0 +1,85 @@
+// Parte A
+// Soma
+// function soma(num, num2){
+// const somar = num + num2
+
+// return `A soma de ${num} + ${num2} é = ${somar}`
+// }
+
+// console.log(soma(10, 5))
+
+
+// Subtração
+// function subtracao(num, num2){
+//     const subtrair = num - num2
+    
+//     return `A subtração de ${num} - ${num2} é = ${subtrair}`
+//     }
+    
+//     console.log(subtracao(10, 5))
+
+
+// Multiplicação
+    // function multiplicacao(num, num2){
+    //     const multiplicar = num * num2
+        
+    //     return `A multiplicação de ${num} * ${num2} é = ${multiplicar}`
+    //     }
+        
+    //     console.log(multiplicacao(10, 5))
+
+
+// Divisão
+        // function divisao(num, num2){
+        //     const dividir = num / num2
+            
+        //     return `A divisão de ${num} / ${num2} é = ${dividir}`
+        //     }
+            
+        //     console.log(divisao(10, 5))
+
+
+            // Parte B
+
+const num = Number(prompt("Digite um valor numérico para A"))
+const num2 = Number(prompt("Digite um valor numérico para B"))
+
+// Parte C
+
+//Soma
+function soma(num, num2){
+const somar = num + num2
+
+return `A soma de ${num} + ${num2} é = ${somar}`
+}
+10
+console.log(soma(num, num2 ))
+
+// Subtração
+function subtracao(num, num2){
+    const subtrair = num - num2
+    
+    return `A subtração de ${num} - ${num2} é = ${subtrair}`
+    }
+    
+    console.log(subtracao(num, num2))
+
+// Multiplicação
+    function multiplicacao(num, num2){
+        const multiplicar = num * num2
+        
+        return `A multiplicação de ${num} * ${num2} é = ${multiplicar}`
+        }
+        
+        console.log(multiplicacao(num, num2))
+
+
+        // Divisão
+        function divisao(num, num2){
+            const dividir = num / num2
+            
+            return `A divisão de ${num} / ${num2} é = ${dividir}`
+            }
+            
+            console.log(divisao(num, num2))
+
