465d9d5f4ec68902aa78307d0d12244a4d29b178
anderavlis <andersonyarro0@gmail.com>
2022-11-23 13:08:18 -0300

    criado funcao para calcular a tabuada a partir da entrada de um numero qualquer

---
+++ exercicio1/script.js
@@ -0,0 +1,49 @@
+// parte A
+
+function digaNome(nome) {
+    return `Olá ${nome}`
+}
+
+console.log(digaNome("Anderson"))
+
+// Parte B
+
+function tabuada(num) {
+    const resTabuada = 
+    console.log(` ${num} x 1=`, num * 1)
+    console.log(` ${num} x 2=`, num * 2)
+    console.log(` ${num} x 3=`, num * 3)
+    console.log(` ${num} x 4=`, num * 4)
+    console.log(` ${num} x 5=`, num * 5)
+    console.log(` ${num} x 6=`, num * 6)
+    console.log(` ${num} x 7=`, num * 7)
+    console.log(` ${num} x 8=`, num * 8)
+    console.log(` ${num} x 9=`, num * 9)
+    console.log(` ${num} x 10=`, num * 10)
+
+return `test ${resTabuada}`
+}
+
+console.log(tabuada(1))
+
+
+// funçao expressao
+const expreTabuada = (numE)=>{
+    const resExpreTabuada = 
+    console.log(` ${numE} x 1=`, numE * 1)
+    console.log(` ${numE} x 2=`, numE * 2)
+    console.log(` ${numE} x 3=`, numE * 3)
+    console.log(` ${numE} x 4=`, numE * 4)
+    console.log(` ${numE} x 5=`, numE * 5)
+    console.log(` ${numE} x 6=`, numE * 6)
+    console.log(` ${numE} x 7=`, numE * 7)
+    console.log(` ${numE} x 8=`, numE * 8)
+    console.log(` ${numE} x 9=`, numE * 9)
+    console.log(` ${numE} x 10=`, numE * 10)
+
+return `${resExpreTabuada}`
+}
+
+console.log(expreTabuada(2))
+
+
